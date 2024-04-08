352b8568c7802ed7111f38be14f2628ebde3718f
Mariana Potichkin <marpotichkin@gmail.com>
2024-04-08 00:42:53 -0300

    favicon changes

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -6,18 +6,18 @@ export const Navbar = () => {
     <nav className="w-full px-[10px] h-20 bg-primary text-primary font-black">
       <div className="flex justify-between items-center max-w-[1440px] mx-auto h-full">
         <div className="flex items-center">
-          <Link className="text-xl lg:text-[32px] w-64 h-auto" href={'/'}>
-            <Image src={'/logo.png'} alt="" width={255} height={51} />
+          <Link className="text-xl lg:text-[32px] " href={'/'}>
+            <Image src={'/logo.png'} alt="" className="w-24 md:w-auto lg:w-[255] h-auto lg:h-[51]" width={255} height={51} />
           </Link>
         </div>
         <ul className="flex gap-4 p-[10px] h-[39px]">
           <li>
-            <Link href={'/auth/new-account'} className="font-bold text-sm md:font-black md:text-base">
+            <Link href={'/auth/new-account'} className="font-bold text-xs md:font-black md:text-base">
               Registrarse
             </Link>
           </li>
           <li>
-            <Link href={'/auth/login'} className="font-bold text-sm md:font-black md:text-base">
+            <Link href={'/auth/login'} className="font-bold text-xs md:font-black md:text-base">
               Iniciar Sesión
             </Link>
           </li>
