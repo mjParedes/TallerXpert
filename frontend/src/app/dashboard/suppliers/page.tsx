bcfcf812fb902cc5e0ffaeff17e7ec9751e46509
David Martinez <dmz.mata@gmail.com>
2024-04-18 14:37:04 -0600

    Changes

--- frontend/src/app/dashboard/suppliers/page.tsx
+++ frontend/src/app/dashboard/suppliers/page.tsx
@@ -1,22 +1,71 @@
 import { Title } from '@/components'
+import { SupplierAccordion } from '@/components/supplier-accordion/SupplierAccordion'
 
 export default function SuppliersPage() {
-  return (
-    <div>
-      <Title title='Página de Proveedores' />
+  const suppliers = [
+    {
+      id: '000-001',
+      company: 'Oster',
+      seller: 'Esteban Lapiedra',
+      phone: '+54 343 4256433',
+      email: 'proveedor@email.com'
+    },
+    {
+      id: '000-002',
+      company: 'GA.MA',
+      seller: 'Lorena Carrigo',
+      phone: '+54 343 252463',
+      email: 'proveedor@email.com'
+    }
+  ]
 
-      <div className='flex justify-between'>
-        <button>Añadir Proveedor</button>
-        <div className='relative w-full min-w-[200px] h-10'>
+  return (
+    <section>
+      <div className='flex justify-between items-end mt-2'>
+        <button className='bg-primary text-white p-3 rounded leading-none'>Añadir Proveedor</button>
+        <div className='relative'>
           <input
-            className='peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900'
-            placeholder=' '
+            type='text'
+            className='rounded border text-black bg-[#fff] pr-6 pl-3 py-1'
+            placeholder='Buscar...'
           />
-          <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
-            Buscar
-          </label>
+          <svg
+            className='absolute top-1/2 right-2 -translate-y-2/4'
+            width='17'
+            height='16'
+            viewBox='0 0 17 16'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <g clip-path='url(#clip0_259_1000)'>
+              <path
+                d='M15.9497 14.2969L12.9258 11.1989C15.1884 8.10235 14.5714 3.71368 11.5478 1.39653C8.52413 -0.920614 4.23873 -0.288803 1.9761 2.80771C-0.286522 5.90423 0.330422 10.2929 3.35408 12.61C5.78293 14.4714 9.11891 14.4714 11.5478 12.61L14.573 15.7081C14.9531 16.0974 15.5695 16.0974 15.9496 15.7081C16.3298 15.3188 16.3298 14.6876 15.9496 14.2983L15.9497 14.2969ZM7.47614 12.0121C4.78886 12.0121 2.61041 9.78114 2.61041 7.02911C2.61041 4.27707 4.78886 2.04613 7.47614 2.04613C10.1634 2.04613 12.3419 4.27707 12.3419 7.02911C12.339 9.7799 10.1622 12.0092 7.47614 12.0121Z'
+                fill='#6264D5'
+              />
+            </g>
+            <defs>
+              <clipPath id='clip0_259_1000'>
+                <rect width='15.6235' height='16' fill='white' transform='translate(0.611816)' />
+              </clipPath>
+            </defs>
+          </svg>
         </div>
       </div>
-    </div>
+
+      <div className='mt-5'>
+        <div className='bg-primary rounded-md flex px-4 py-4 gap-2 shadow-[0_4px_12px_0_rgba(0,0,0,0.25)] mb-5'>
+          <div className='text-white font-bold text-sm flex-1'>Id Proveedor</div>
+          <div className='text-white font-bold text-sm flex-1'>Empresa</div>
+          <div className='text-white font-bold text-sm flex-1'>Vendedor</div>
+          <div className='text-white font-bold text-sm flex-1'>Teléfono</div>
+          <div className='text-white font-bold text-sm flex-1'>Email</div>
+          <span className='flex-1'></span>
+        </div>
+
+        {suppliers.map((supplier, index) => (
+          <SupplierAccordion key='index' supplier={supplier} />
+        ))}
+      </div>
+    </section>
   )
 }
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -32,4 +32,3 @@ export { TechniciansDark } from './icons/TechniciansDark'
 
 // form create workshop
 export * from './create-taller/FormCreateWorkshop'
-
