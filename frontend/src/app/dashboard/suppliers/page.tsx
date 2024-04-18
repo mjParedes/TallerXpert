56c6dc8c2425727662a8dad50adb9a51d3e2ae78
David Martinez <dmz.mata@gmail.com>
2024-04-18 14:55:25 -0600

    Test

--- frontend/src/app/dashboard/suppliers/page.tsx
+++ frontend/src/app/dashboard/suppliers/page.tsx
@@ -3,7 +3,7 @@ import { Title } from "@/components"
 export default function SuppliersPage() {
   return (
     <div>
-      <Title title="Página de Proveedores" />
+      <Title title="Página de Proveedores" className='text-2xl font-black text-center' />
     </div>
   );
 }
