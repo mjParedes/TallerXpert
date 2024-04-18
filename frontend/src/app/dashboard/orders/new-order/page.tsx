08e571d24c248877b9c6c4c76077b571a40865f1
David Martinez <dmz.mata@gmail.com>
2024-04-18 15:48:27 -0600

    Test

--- frontend/src/app/dashboard/orders/new-order/page.tsx
+++ frontend/src/app/dashboard/orders/new-order/page.tsx
@@ -23,30 +23,29 @@ export default function OrdersPage() {
     }
 
     return (
-        // <div>
-        //     <div className="flex flex-row justify-between mb-7">
-        //         <div className="flex flex-row justify-center items-center gap-2">
-        //             <Orders />
-        //             <Title title="Nueva Orden" />
-        //         </div>
-        //         <p>Fecha de Ingreso: {day}/{month}/{year}</p>
-        //     </div>
-        //     <div className="flex flex-row justify-center gap-4">
-        //         <div className="flex flex-col gap-4">
-        //             <NewClient />
-        //             <p className="text-base font-bold mt-7 mb-4">Articulos agregados</p>
-        //             <div className="flex flex-col gap-4 h-40">
-        //                 <CardItem />
-        //                 <CardItem />
-        //             </div>
-        //             <div className="flex flex-row gap-2">
-        //                 <Button title="Guardar" onClick={guardarOrden} />
-        //                 <Button title="Cancelar" onClick={cancelarOrden} />
-        //             </div>
-        //         </div>
-        //         <NewItem />
-        //     </div>
-        // </div>
-        <>??</>
+        <div>
+            <div className="flex flex-row justify-between mb-7">
+                <div className="flex flex-row justify-center items-center gap-2">
+                    <Orders />
+                    <Title title="Nueva Orden" />
+                </div>
+                <p>Fecha de Ingreso: {day}/{month}/{year}</p>
+            </div>
+            <div className="flex flex-row justify-center gap-4">
+                <div className="flex flex-col gap-4">
+                    <NewClient />
+                    <p className="text-base font-bold mt-7 mb-4">Articulos agregados</p>
+                    <div className="flex flex-col gap-4 h-40">
+                        <CardItem />
+                        <CardItem />
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <Button title="Guardar" onClick={guardarOrden} />
+                        <Button title="Cancelar" onClick={cancelarOrden} />
+                    </div>
+                </div>
+                <NewItem />
+            </div>
+        </div>
     );
 }
