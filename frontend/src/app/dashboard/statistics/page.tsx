582c8aa95e9ba9546d6a20ef3740a624a3daa91a
lags2022 <lguzman.58erb@outlook.com>
2024-04-24 23:00:00 -0500

    feat: resolviendo conflictos 2

--- frontend/src/app/dashboard/statistics/page.tsx
+++ frontend/src/app/dashboard/statistics/page.tsx
@@ -1,9 +1,11 @@
-import { Title } from "@/components"
+import { Title } from "@/components";
+import { TableComponent } from "./components/Table.component";
 
 export default function StatisticsPage() {
   return (
-    <div>
+    <div className="space-y-8">
       <Title title="Página de Estadísticas" />
+      <TableComponent />
     </div>
   );
 }
